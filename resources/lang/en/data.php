96cc28fbc8e8a89d7040849fc20685ce6567122c
islommuhammad <salomshox@gmail.com>
2021-09-21 17:56:48 +0500

    git added

--- resources/lang/en/data.php
+++ resources/lang/en/data.php
@@ -81,8 +81,8 @@ return [
                 <p>XR: 23402000300100001010</p>
                 <p>The Central Bank of the Republic of Uzbekistan</p>
                 <p>MFO 00014 STIR 201122919</p>
-                <p>SHXR: 400910860184017094100078001</p>
-                <p>STIR 205773414 OKONX 80300</p>',
+                <p>SHXR: 400910860184017950100078001</p>
+                <p>STIR 205773414 OKONX 92110</p>',
         'grand' => '
                 <h4>TUIT Samarkand branch (Budget)</h4>
                 <p>XR: 23402000300100001010</p>
