a5b16686fc063da3f7219962796841e44327d7d9
Alijon MrC <alijonmrc99@gmail.com>
2021-12-14 10:01:42 +0500

    last

--- resources/views/frontend/layouts/quick-menu.blade.php
+++ resources/views/frontend/layouts/quick-menu.blade.php
@@ -1,11 +1,11 @@
 <nav>
     <h4>{{__('data.quick-menu-title')}}</h4>
     <ul>
-        <li><a href="https://samtuit.uz/uz/page/faculties-11-08-2021">{{__('data.quick-menu.faculties')}}</a></li>
-        <li><a href="https://samtuit.uz/uz/page/departments-11-08-2021">{{__('data.quick-menu.departments')}}</a></li>
-        <li><a href="https://samtuit.uz/uz/page/departments-and-centers-11-08-2021">{{__('data.quick-menu.centres-and-departments')}}</a></li>
-        <li><a href="https://samtuit.uz/uz/page/instructions-11-08-2021">{{__('data.quick-menu.entrant')}}</a></li>
-        <li><a href="https://samtuit.uz/uz/page/scientific-research-activities-11-08-2021">{{__('data.quick-menu.students')}}</a></li>
-        <li><a href="https://samtuit.uz/uz/page/research-directions-11-08-2021">{{__('data.quick-menu.scientific-activity')}}</a></li>
+        <li><a href="/page/faculties-11-08-2021">{{__('data.quick-menu.faculties')}}</a></li>
+        <li><a href="/page/departments-11-08-2021">{{__('data.quick-menu.departments')}}</a></li>
+        <li><a href="/page/departments-and-centers-11-08-2021">{{__('data.quick-menu.centres-and-departments')}}</a></li>
+        <li><a href="/page/instructions-11-08-2021">{{__('data.quick-menu.entrant')}}</a></li>
+        <li><a href="/page/scientific-research-activities-11-08-2021">{{__('data.quick-menu.students')}}</a></li>
+        <li><a href="/page/research-directions-11-08-2021">{{__('data.quick-menu.scientific-activity')}}</a></li>
     </ul>
 </nav>
